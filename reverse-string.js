02bcd9945c0632cd7a152cc0708dd9f6a474e383
Phillipe Rodriguez <philliperod@gmail.com>
2020-11-02 12:44:08 -0600

    completed solution: pass the input into an array, reverse the array, then joined the values in an array

--- reverse-string.js
+++ reverse-string.js
@@ -1,8 +1,7 @@
-//
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const reverseString = () => {
-  throw new Error("Remove this statement and implement this function");
+export const reverseString = (string) => {
+  return string.split('').reverse().join('');
 };
+
+// 1) take the input and put it into an array
+// 2) reverse the values in the array
+// 3) join the reversed values in the array
